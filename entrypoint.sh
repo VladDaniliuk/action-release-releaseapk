15d099c76c58b74dff0dd3dcef118ebc6285d588
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2021-08-04 21:15:30 +0300

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "v${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
+hub checkout ${${REPO_BRANCH}:-master}
+VERSION_NAME=`grep -E "${VERSION_VARIABLE}" ./${APP_FILE} | grep -oE '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'`
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}"
