770182e75d5b271d4e04c7a569e67f3cb0e55e9e
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2021-08-04 21:56:43 +0300

    Delete hub checkout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -E "${VERSION_VARIABLE}" ./${APP_FILE} | grep -oE '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}'`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}"
