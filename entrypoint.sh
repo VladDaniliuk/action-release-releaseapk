bdd1bdf82679c13dfc3491bdfefc87a690f0ca8c
Vladislav Daniliuk <48360685+VladDaniliuk@users.noreply.github.com>
2022-04-28 12:03:58 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 VERSION_NAME=`grep -E "${VERSION_VARIABLE}" ${APP_FILE} | grep -oE '([0-9]{1,}\.)+[0-9]{1,}'`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/"Unlim Storage.apk" -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${VERSION_NAME}
+NAME = `grep -E "${NAME_VARIABLE}" ${APP_FILE} | grep -oP '"\K.*?(?=")'`
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/"${NAME}-release.apk" -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${VERSION_NAME}
