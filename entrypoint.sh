e00f64163dbf1f64376373259e0702df2156ca0f
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-11-01 18:02:44 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-VERSION_NAME=`grep -E "${VERSION_VARIABLE}" ${APP_FILE} | grep -oE '([0-9]{1,}\.)+[0-9]{1,}'`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/"${NAME_VARIABLE}" -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${VERSION_NAME}
+VERSION_NAME=`grep -E "versionName" ${APP_FILE} | grep -oE '([0-9]{1,}\.)+[0-9]{1,}'`
+hub release create -a "${APP_FOLDER}" -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${VERSION_NAME}
