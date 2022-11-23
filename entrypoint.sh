380b76cd881b705996edd3fa70e0ee5c22d4ddda
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-11-23 16:50:21 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 VERSION_NAME=`grep -E "versionName" ${APP_FILE} | grep -oE '([0-9]{1,}\.)+[0-9]{1,}'`
-hub release create -a "${APP_FOLDER}" -m "${RELEASE_TITLE} - v${VERSION_NAME}" ${VERSION_NAME}
+MESSAGE= `sed  '1,/## [${VERSION_NAME}]/d;/## [.*]/Q' ${CHANGELOG_FILE}`
+hub release create -a "${APP_FOLDER}" -m "${RELEASE_TITLE} - v${VERSION_NAME} /n${MESSAGE}" ${VERSION_NAME}
