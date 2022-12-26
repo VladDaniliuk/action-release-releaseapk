b7d927e4b936ec9c6e7852553d644a34b883ff4b
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-12-26 19:06:37 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 VERSION_NAME=`grep -E "versionName" ${APP_FILE} | grep -oE '([0-9]{1,}\.)+[0-9]{1,}'`
-MESSAGE=$(awk -v ver="$VERSION_NAME" '/^#+ \[/ { if (p) { exit }; if ($CHANGELOG_FILE == "["ver"]") { p=1; next} } p && NF' "$2")
+MESSAGE=$(awk -v ver="$VERSION_NAME" '/^#+ \[/ { if (p) { exit }; if ($CHANGELOG_FILE == "["ver"]") { p=1; next} } p && NF' "$CHANGELOG_FILE")
 hub release create -a "${APP_FOLDER}" -m "${RELEASE_TITLE} - v${VERSION_NAME} /n${MESSAGE}" ${VERSION_NAME}
